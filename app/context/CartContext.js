f3d8f3482b4e5ad9a08f2a3f73e997698077e69d
Twill1007 <thomaswillford@gmail.com>
2025-07-08 14:50:52 -0700

    Updated local storage

--- app/context/CartContext.js
+++ app/context/CartContext.js
@@ -1,10 +1,21 @@
 "use client";
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const CartContext = createContext();
 
 export function CartProvider({ children }) {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(() => {
+    if (typeof window !== "undefined") {
+      const storedCart = localStorage.getItem("cart");
+      return storedCart ? JSON.parse(storedCart) : [];
+    }
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+
   const [checkoutInfo, setCheckoutInfo] = useState(null);
 
   return (
