ee21f9564387ce7da590b8e2778e1fbb88274620
Twill1007 <thomaswillford@gmail.com>
2025-07-09 15:09:34 -0700

    Updated price v2

--- app/constants/quantityOptions.js
+++ app/constants/quantityOptions.js
@@ -1,7 +1,7 @@
 const quantityOptions = [
-  { label: "1 dozen ($10)", value: 1, price: 12 },
-  { label: "2 dozen ($18)", value: 2, price: 20 },
-  { label: "3 dozen ($24)", value: 3, price: 26 },
+  { label: "1 dozen ($12)", value: 1, price: 12 },
+  { label: "2 dozen ($20)", value: 2, price: 20 },
+  { label: "3 dozen ($26)", value: 3, price: 26 },
 ];
 
 export default quantityOptions;
